2d9bd6fa60e4e18a3e8493a76aa13ab5293988f9
junggeyy <jung.vickey@gmail.com>
2025-03-04 12:56:15 -0500

    cors url update

--- frontend/bot/src/App.js
+++ frontend/bot/src/App.js
@@ -20,7 +20,7 @@ function App() {
 
         setLoading(true);
 
-        fetch(`${apiURL}`, {
+        fetch(`${apiURL}chat`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ user_id: "test_user", message }),
